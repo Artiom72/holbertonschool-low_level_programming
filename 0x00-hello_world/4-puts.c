524167d33b908072b89e05be3cb7967b9fb8ded6
Artiom72 <adaf7576@gmail.com>
2021-02-04 00:18:36 +0000

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- Entry point
+ *
+ *Return: Output a message using puts
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
