6b2dde6cb2a096a4a72bdb6ebe9ba24be09a58f9
Artiom72 <adaf7576@gmail.com>
2021-02-05 03:11:20 +0000

    first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - Print numbers 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9 ; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
