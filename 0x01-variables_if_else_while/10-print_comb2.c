8ca767345578901abd13a7a6139566c796ae6eb7
Artiom72 <adaf7576@gmail.com>
2021-02-05 05:43:46 +0000

    first commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - Print numbers base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, i;
+
+	for (n = 0; n <= 9 ; n++)
+{
+		for (i = 0; i <= 9 ; i++)
+{
+			putchar(n + '0');
+			putchar(i + '0');
+			if (n != 9 || i != 9)
+{
+				putchar(',');
+				putchar(' ');
+}
+}
+}
+	putchar ('\n');
+	return (0);
+}
